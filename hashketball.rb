618abd23df9dd6c3ba81688c8f90957d022d98e5
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-14 04:23:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -321,7 +321,7 @@ def player_with_most_steals
     end
   end
 end
-binding.pry
-# def long_name_steals_a_ton
-#   if
-# end
+
+def long_name_steals_a_ton
+  player_with_most_steals == player_with_longest_name
+end
